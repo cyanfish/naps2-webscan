4571e9c917edcc053f89aaeba047725529fdc7bf
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-08-17 14:48:17 -0700

    Working scanning from browser

--- NAPS2.WebScan.LocalService/Worker.cs
+++ NAPS2.WebScan.LocalService/Worker.cs
@@ -21,15 +21,24 @@ public class Worker : BackgroundService
         using var scanningContext = new ScanningContext(new ImageSharpImageContext());
         var controller = new ScanController(scanningContext);
 
-        var devices = await controller.GetDeviceList();
-        var first = devices.First();
-
         using var scanServer = new ScanServer(scanningContext, new EsclServer
         {
+            // This line is required for scanning from a browser to work
             SecurityPolicy = EsclSecurityPolicy.ServerAllowAnyOrigin
         });
-        scanServer.RegisterDevice(first, port: 9880);
 
+        // Pick the first WIA device to share; you might want to do something else here
+        // depending on your use case.
+        //
+        // Note: Driver.Twain currently doesn't work from a Windows Service
+        var devices = await controller.GetDeviceList(Driver.Wia);
+        var firstDevice = devices.First();
+
+        // You'll need to manually specify a port as device discovery doesn't work from a browser.
+        // If you're sharing multiple devices make sure to give them each a unique port number.
+        scanServer.RegisterDevice(firstDevice, port: 9880);
+
+        // Share the device(s) until the service is stopped
         await scanServer.Start();
         await stoppingToken.WaitHandle.WaitOneAsync();
         await scanServer.Stop();
---
+++ NAPS2.WebScan.WebServer/wwwroot/js/site.ts
@@ -0,0 +1,38 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+import Scanner from '../lib/escl-sdk-ts/escl/scanner'
+
+document.getElementById('scan-button').addEventListener('click', async e => {
+    console.log('Starting scan attempt');
+
+    const ip = '127.0.0.1';
+    const port = 9880;
+    const scanner = new Scanner({ip, port});
+
+    try {
+        const capabilities = await scanner.ScannerCapabilities();
+        console.log('scanner ScannerCapabilities', capabilities)
+
+        const jobUrl = await scanner.ScanJobs({
+            Resolution: 300
+        });
+        console.log('scanner ScanJobs', jobUrl);
+
+        const jobId = jobUrl.split('/').at(-1);
+
+        const doc = await scanner.NextDocument(jobId);
+        console.log('scanner NextDocument', doc);
+
+
+        var blob = new Blob([doc.data], {type: "image/jpeg"});
+        var imageUrl = window.URL.createObjectURL(blob);
+        var img = document.querySelector("#preview") as HTMLImageElement;
+        img.src = imageUrl;
+
+    } catch (err) {
+        console.log('scanner err', err.response)
+    }
+});
